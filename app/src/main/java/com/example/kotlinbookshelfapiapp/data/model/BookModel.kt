dd0184188f18133271c55006c9ec8f1241d1e4ce
Joeltt <joelttm15@gmail.com>
2025-03-31 09:11:21 +0100

    fix: fix fields' typos in bookModel data class

--- app/src/main/java/com/example/kotlinbookshelfapiapp/data/model/BookModel.kt
+++ app/src/main/java/com/example/kotlinbookshelfapiapp/data/model/BookModel.kt
@@ -4,8 +4,8 @@ data class BookModel(
     val id: String,
     val title: String,
     val authors: List<String>,
-    val publishedData: String,
-    val category: List<String>,
+    val publishedDate: String,
+    val categories: List<String>,
     val pageCount: Int,
     val image: ImageLinks
 )
