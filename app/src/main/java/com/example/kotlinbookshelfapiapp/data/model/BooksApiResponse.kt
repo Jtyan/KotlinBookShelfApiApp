0198ff928428340de1388ef9bc54c2d0a3e44d8a
Joeltt <joelttm15@gmail.com>
2025-04-07 14:16:19 +0100

    feat: reduce model fields and add @Serializable annotations for Kotlinx Serialization

--- app/src/main/java/com/example/kotlinbookshelfapiapp/data/model/BooksApiResponse.kt
+++ app/src/main/java/com/example/kotlinbookshelfapiapp/data/model/BooksApiResponse.kt
@@ -1,28 +1,29 @@
 package com.example.kotlinbookshelfapiapp.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BooksApiResponse(
-    val kind: String,
-    val totalItems: String,
-    val items: List<BookVolume>
+    val items: List<BookVolume>? = emptyList()
 )
 
+@Serializable
 data class BookVolume(
     val id: String,
     val volumeInfo: VolumeInfo
 )
 
+@Serializable
 data class VolumeInfo(
     val title: String,
-    val authors: List<String>,
-    val description: String,
-    val publisher: String,
-    val publishedDate: String,
-    val pageCount: Int,
-    val categories: List<String>,
-    val imageLinks: ImageLinks,
-    val language: String
+    val authors: List<String>? = emptyList(),
+    val publishedDate: String? = null,
+    val categories: List<String>? = emptyList(),
+    val pageCount: Int? = null,
+    val imageLinks: ImageLinks? = ImageLinks("", "")
 )
 
+@Serializable
 data class ImageLinks(
     val smallThumbnail: String,
     val thumbnail: String
