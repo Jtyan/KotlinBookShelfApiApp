21311c138e77889f0147c36a6257294d98cdac86
Joeltt <joelttm15@gmail.com>
2025-04-07 14:22:42 +0100

    feat: define BookModel and add String extension function to convert http to https

---
+++ app/src/main/java/com/example/kotlinbookshelfapiapp/domain/model/BookModel.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinbookshelfapiapp.domain.model
+
+import com.example.kotlinbookshelfapiapp.data.model.ImageLinks
+
+data class BookModel(
+    val id: String,
+    val title: String,
+    val authors: List<String>,
+    val publishedDate: String,
+    val categories: List<String>,
+    val pageCount: Int,
+    val image: ImageLinks
+)
+
+fun String.toHttps(): String {
+    return this.replace("http://", "https://")
+}
